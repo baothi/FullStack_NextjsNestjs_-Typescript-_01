3287517d0103e7f77d034337bdf8bc4ed3ee7d8c
baothi <baothi246@gmail.com>
2024-09-16 09:32:29 +0700

    create project backend and fontend

---
+++ 01-next-rm/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      hello word nextjs
+      <div>
+        <Button type="primary">hello</Button>
+      </div>
+    </div>
+  );
+}
