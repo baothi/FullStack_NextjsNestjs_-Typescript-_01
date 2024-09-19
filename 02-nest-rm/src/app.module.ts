0793702cf1b66be6d2594056d152558f90f3db37
baothi <baothi246@gmail.com>
2024-09-19 08:58:47 +0700

    add all table

--- 02-nest-rm/src/app.module.ts
+++ 02-nest-rm/src/app.module.ts
@@ -1,13 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { UsersModule } from '@/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { LikesModule } from '@/likes/likes.module';
+import { MenuItemOptionsModule } from '@/menu.item.options/menu.item.options.module';
+import { MenuItemsModule } from '@/menu.items/menu.items.module';
+import { MenusModule } from '@/menus/menus.module';
+import { OrderDetailModule } from '@/order.detail/order.detail.module';
+import { OrdersModule } from '@/orders/orders.module';
+import { RestaurantsModule } from '@/restaurants/restaurants.module';
+import { ReviewsModule } from '@/reviews/reviews.module';
 
 @Module({
   imports: [
     UsersModule,
+    LikesModule,
+    MenuItemOptionsModule,
+    MenuItemsModule,
+    MenusModule,
+    OrderDetailModule,
+    OrdersModule,
+    RestaurantsModule,
+    ReviewsModule,
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
