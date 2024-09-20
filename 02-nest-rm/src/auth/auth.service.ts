5b70573817637a5f4864c143981b91b260bbc383
baothi <baothi246@gmail.com>
2024-09-20 15:27:01 +0700

    login and get profile

--- 02-nest-rm/src/auth/auth.service.ts
+++ 02-nest-rm/src/auth/auth.service.ts
@@ -10,15 +10,19 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async signIn(email: string, pass: string): Promise<any> {
+  async validateUser(email: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(email);
     const isValidPassword = await comparePasswordHelper(pass, user.password);
-    if (!isValidPassword) {
-      throw new UnauthorizedException(`Invalid email ${email} and password ${pass}`);
-    }
-    const payload = { sub: user._id, email: user.email };
+
+    if (!user || !isValidPassword) return null;
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.email, sub: user._id, role: user.role };
     return {
-      access_token: await this.jwtService.signAsync(payload),
+      access_token: this.jwtService.sign(payload),
     };
   }
+
 }
---
+++ 02-nest-rm/src/auth/passport/local.strategy.ts
@@ -0,0 +1,22 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'email', // This tells passport to use 'email' instead of 'username'
+      passwordField: 'password', // In case you want to rename 'password' field
+    });
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authService.validateUser(username, password);
+    if (!user) {
+      throw new UnauthorizedException(`Invalid email ${username} and password ${password}`);
+    }
+    return user;
+  }
+}
