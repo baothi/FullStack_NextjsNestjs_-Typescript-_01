a53949c9a9abbfe2f08804ceb92bd5653eb387f8
baothi <baothi246@gmail.com>
2024-09-23 07:11:24 +0700

    add layout dashboard

---
+++ 01-next-rm/src/components/layout/admin.header.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { Layout } from 'antd';
+import React from 'react'
+
+const AdminHeader = () => {
+    const { Header } = Layout;
+  return (
+    <>
+        <Header style={{ padding: 0, background: '#ffffff' }} />
+    </>
+  )
+}
+
+export default AdminHeader;
